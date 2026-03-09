5a79d478e2c7d2ab2302b336e7f04b190e9bc16d
Mohamed Mamdouh <eng.mohamed.mamdouh@gmail.com>
2026-03-09 23:20:30 +0000

    write example

--- src/agent.py
+++ src/agent.py
@@ -95,14 +95,6 @@ def prewarm(proc: JobProcess):
 
 server.setup_fnc = prewarm
 
-def calculate_cost():
-    return 2
-
-# soundflare = LivekitObserve(
-#     agent_id="60ffcbb4-1c12-47c5-9849-de2e6a6990e1",
-#     apikey="soundflare_c7ab3d6be7ee1299587806d113755181d762bc44ed943f087b305be42291f1b0" # Generate this in the SoundFlare Dashboard
-# )
-
 
 @server.rtc_session(agent_name="my-agent")
 async def my_agent(ctx: JobContext):
@@ -142,34 +134,10 @@ async def my_agent(ctx: JobContext):
   )
 
 
-
-    # session_id = soundflare.start_session(session=session)
+    def calculate_cost(usage):
+        return 2 * usage.llm_prompt_tokens / 1000 + 4 * usage.llm_completion_tokens / 1000
     
-    # # Export data on shutdown
-    # async def on_shutdown():
-    #     await soundflare.export(session_id)
-    # ctx.add_shutdown_callback(on_shutdown)
     
-    # await session.start(...)
-
-    # To use a realtime model instead of a voice pipeline, use the following session setup instead.
-    # (Note: This is for the OpenAI Realtime API. For other providers, see https://docs.livekit.io/agents/models/realtime/))
-    # 1. Install livekit-agents[openai]
-    # 2. Set OPENAI_API_KEY in .env.local
-    # 3. Add `from livekit.plugins import openai` to the top of this file
-    # 4. Use the following session setup instead of the version above
-    # session = AgentSession(
-    #     llm=openai.realtime.RealtimeModel(voice="marin")
-    # )
-
-    # # Add a virtual avatar to the session, if desired
-    # # For other providers, see https://docs.livekit.io/agents/models/avatar/
-    # avatar = hedra.AvatarSession(
-    #   avatar_id="...",  # See https://docs.livekit.io/agents/models/avatar/plugins/hedra
-    # )
-    # # Start the avatar and wait for it to join
-    # await avatar.start(session, room=ctx.room)
-
     # Create the assistant with configured function tools.
     assistant = Assistant()
 
